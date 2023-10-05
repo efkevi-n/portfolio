ff6c65ae18b737dc478aa2208e43a8cf95f3c5f6
Enoch Flourish <flourzeez@gmail.com>
2023-10-05 18:19:19 +0300

    commit

--- components/project.tsx
+++ components/project.tsx
@@ -27,15 +27,17 @@ function Project({
   imageUrl,
 }: ProjectProps) {
   return (
-    <article className=" bg-gray-200 max-w-[42rem] boder border-black/[5] overflow-hidden sm:pr-8 relative">
-      <h3 className="text-2xl font-semibold">{title}</h3>
+    <article className=" bg-gray-200 max-w-[42rem] boder border-black/[5] overflow-hidden sm:pr-8 relative sm:h-[20rem]">
+    <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] ">
+    <h3 className="text-2xl font-semibold">{title}</h3>
       <p className="mt-4 leading-relaxed text-gray-700 ">{description}</p>
       <ul className="flex flex-wrap mt-4 gap-3"> 
         {tags.map((tag, index) => (
           <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full " key={index}>{tag}</li>
         ))}
       </ul>
-      <Image src={imageUrl} alt="projects" quality={100} className= "absolute"/>
+    </div>
+      <Image src={imageUrl} alt="projects" quality={100} className= "absolute top-8 -right-40 w-[28.3rem] rounded-t-lg shadow-2xl"/>
     </article>
   );
 }
