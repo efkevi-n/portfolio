eb388e32d2e38c40d46fb22fd4f171d58bc13ec2
Enoch Flourish <flourzeez@gmail.com>
2023-10-05 14:56:35 +0300

    setting up projects

--- components/project.tsx
+++ components/project.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import SectionHeader from './section-header'
+import { projectsData } from '@/lib/data'
 
 export default function Projects() {
   return (
    <section>
     <SectionHeader> Projects </SectionHeader>
+    <div>
+      {
+        projectsData.map((project, index)=> (
+            <Project key={index} />
+        ))
+      }
+    </div>
    </section>
   )
+}
+
+function Project() {
+ return <div></div>
 }
