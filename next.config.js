6d848fa031d4e875a4cd675941623990da93fdc3
Enoch Flourish <flourzeez@gmail.com>
2023-10-11 18:36:54 +0300

    commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     
-}
-
-module.exports = nextConfig
-
-module.exports = {
   images: {
-    domains: ['localhost'], // Add domain names where your images will be served from
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
   },
 };
+
+module.exports = nextConfig;
