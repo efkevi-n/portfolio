2a6fc67cf516c88ef22b1707a806e94870cdbecb
Enoch Flourish <flourzeez@gmail.com>
2023-10-11 19:37:22 +0300

    commit

--- lib/data.ts
+++ lib/data.ts
@@ -44,17 +44,17 @@ export const experiencesData = [
   },
   {
     title: "Front-End Developer",
-    location: "Ankara, Turkey",
+    location: "Indiana, USA",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2022- 2023",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Indiana, USA",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
     date: "2021 - present",
   },
