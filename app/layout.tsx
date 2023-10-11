07078c0cacea89c0601c0ac7e75901794b476f79
Enoch Flourish <flourzeez@gmail.com>
2023-10-12 02:26:15 +0300

    made activesection provider work

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 const inter = Inter({ subsets: ['latin'] })
+import ActiveSectionContextProvider from '@/context/active-section-context'
+import Footer from '@/components/footer'
+import ThemeSwitch from '@/components/theme'
+import ThemeContextProvider from '@/context/theme-content'
+import { Toaster } from 'react-hot-toast'
 
 export const metadata = {
   title: 'Enochs | Portfolio',
@@ -18,8 +23,17 @@ export default function RootLayout({
       <body className={`${inter.className} bg-slate-100 text-gray-950 relative pt-28 sm:pt-36` }>
         <div className= "bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] " ></div>
         <div className= "bg-[#dbd7fb] absolute top-[-6rem] -z-10 left-[-35rem] h-[30rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" ></div>
-      <Header />
-        {children}</body>
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
+        </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
