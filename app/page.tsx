cef96d1d5bc0d6ecf69a63895efc2641c73f9663
Enoch Flourish <flourzeez@gmail.com>
2023-11-09 20:35:17 +0300

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SectionDivider from "@/components/section-divider";
 import Info from "@/components/info";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
-import Experience from "@/components/experience";
+// import Experience from "@/components/experience";
 import Contact from "@/components/contact";
 
 
